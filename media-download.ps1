530472218d3fd9de3ba7f819128816200c362038
Joseph Davis <joedbns@gmail.com>
2024-03-18 00:36:34 -0400

    Add initial version of media download script

---
+++ media-download.ps1
@@ -0,0 +1,18 @@
+#powershell.exe -File "____________.ps1" --url "https://i.4cdn.org/hr/1704332058600653.jpg" --name "building-by-water"
+
+param (
+    [string] $url = "https://i.4cdn.org/hr/1704332058600653.jpg",
+    [Parameter(ParameterSetName="name")]
+    [string] $overwriteName = ""
+)
+
+$fileNameFull = ($url -split "/")[-1]
+# $fileName, $fileExtention = ($fileNameFull -split "\.")[0, -1]
+
+if ($overwriteName -eq "") {
+    $overwriteName = ($fileNameFull -split "\.")[0]
+}
+
+$wc = New-Object System.Net.WebClient
+
+$wc.DownloadFile($url, "C:\temp\$fileNameFull")
